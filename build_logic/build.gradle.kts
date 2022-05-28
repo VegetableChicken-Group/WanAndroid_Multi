723a26e7b7073e68300dafccb144d02368be6c84
985892345 <2767465918@qq.com>
2022-05-28 20:19:40 +0800

    [feature]升级gradle至最新版本

--- build_logic/build.gradle.kts
+++ build_logic/build.gradle.kts
@@ -3,12 +3,14 @@ plugins {
 }
 
 repositories {
+  maven { url = uri("https://maven.aliyun.com/repository/public") }
+  maven { url = uri("https://maven.aliyun.com/repository/google") }
   mavenCentral()
   google()
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:7.1.3")
+  implementation("com.android.tools.build:gradle:7.2.1")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 }
 
