8d5898a372e9b1e4801741b0ef3d954c861cd1c7
RQ527 <Ran_Sixiang@163.com$ ssh-keygen -t rsa -C Ran_Sixiang@163.comqgit config --global user.email Ran_Sixiang@163.com>
2022-05-30 23:26:09 +0800

    [feature]构建module_publicaccounts模块

--- module_publicaccounts/build.gradle.kts
+++ module_publicaccounts/build.gradle.kts
@@ -2,13 +2,13 @@ import lib.dependAndroidKtx
 import lib.dependAndroidView
 
 plugins {
-  id("module-manager")
-//  id("module-debug")
+  //  id("module-manager")
+  id("module-debug")
 }
 
 dependAndroidView()
 dependAndroidKtx()
 
 dependencies {
-  // 这里面写该只有自己模块才会用到的依赖
+    // 这里面写该只有自己模块才会用到的依赖
 }
---
+++ module_publicaccounts/src/main/java/com/ndhzs/module/publicaccounts/WeChatMainActivity.kt
@@ -0,0 +1,13 @@
+package com.ndhzs.module.publicaccounts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ndhzs.lib.common.ui.BaseActivity
+import com.ndhzs.module.publicaccounts.R
+
+class WeChatMainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_we_chat_main)
+    }
+}
