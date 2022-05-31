bde268d731384d8aae5c7efad3341cead76cf86d
Mr-than <64783138+Mr-than@users.noreply.github.com>
2022-06-01 01:51:59 +0800

    创建好了各个页面的模块

---
+++ module_project/src/main/java/com/example/module/project/ProjectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.module.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project)
+    }
+}
