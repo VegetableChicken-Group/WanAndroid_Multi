c4cf5828e6b5e8b67c250ca380c480cd0c54cd5d
985892345 <2767465918@qq.com>
2022-05-31 11:06:53 +0800

    [test]尝试执行CI

--- module_test/src/main/java/com/ndhzs/module/test/page/TestActivity.kt
+++ module_test/src/main/java/com/ndhzs/module/test/page/TestActivity.kt
@@ -30,5 +30,6 @@ class TestActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.test_activity_test)
+    val name = mData.name
   }
 }
