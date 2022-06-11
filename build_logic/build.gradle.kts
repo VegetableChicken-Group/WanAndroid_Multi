afb1a22210a9bbb410277789acbe06cec3a0b707
985892345 <2767465918@qq.com>
2022-06-11 23:24:00 +0800

    [fix]回退kt至1.6.21

--- build_logic/build.gradle.kts
+++ build_logic/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
   implementation("com.android.tools.build:gradle:7.2.1")
-  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+  implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 }
 
 // 插件的定义
