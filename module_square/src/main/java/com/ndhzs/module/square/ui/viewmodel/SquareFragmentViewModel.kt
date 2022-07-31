da565a3a7db60a853cb2f709f446a30cd052ecc8
Silence <92719733+CM-Silence@users.noreply.github.com>
2022-07-31 16:55:10 +0800

    [feature]SquareFragment所使用的ViewModel

---
+++ module_square/src/main/java/com/ndhzs/module/square/ui/viewmodel/SquareFragmentViewModel.kt
@@ -0,0 +1,14 @@
+package com.ndhzs.module.square.ui.viewmodel
+
+import com.ndhzs.lib.common.ui.BaseViewModel
+import com.ndhzs.module.square.net.SquareApiService
+
+/**
+ * @ClassName SquareFragmentViewModel
+ * @author Silence~
+ * @date 2022/7/31
+ * @Description
+ */
+class SquareFragmentViewModel : BaseViewModel() {
+
+}
